a83589cdcea5a0599e2c1a74fabce62b2fa630c5
JongWonLee <BruceFreedy@gmail.com>
2021-09-19 21:40:29 +0900

    commit

--- src/main/java/kr/jongwonlee/fmg/proc/FileParser.java
+++ src/main/java/kr/jongwonlee/fmg/proc/FileParser.java
@@ -105,7 +105,7 @@ public class FileParser {
         if (string == null) return -1;
         for (int i = 0; i < string.length(); i++) {
             if (procBraces.containsKey(string.charAt(i))) {
-                return i == 0 ? -1 : i;
+                return i == 0 ? 1 : i;
             }
         }
         return -1;
