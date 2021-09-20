19c2b50e84258c40b79c01b5a676061ce3f00473
JongWonLee <BruceFreedy@gmail.com>
2021-09-20 12:33:22 +0900

    commit

---
+++ src/main/java/kr/jongwonlee/fmg/proc/data/control/SingleQuotationEscape.java
@@ -0,0 +1,26 @@
+package kr.jongwonlee.fmg.proc.data.control;
+
+import kr.jongwonlee.fmg.game.MiniGame;
+import kr.jongwonlee.fmg.proc.Process;
+import kr.jongwonlee.fmg.proc.*;
+
+@Processable(alias = "\\'")
+public class SingleQuotationEscape implements Process {
+
+    Process process;
+
+    @Override
+    public void parse(ParseUnit parseUnit, String arguments) {
+        process = FileParser.parseProcess(parseUnit, arguments);
+    }
+
+    @Override
+    public String run(MiniGame miniGame, ProcUnit procUnit) {
+        return "'" + process.run(miniGame, procUnit);
+    }
+
+    @Override
+    public ProcType getType() {
+        return ProcType.SINGLE_QUOTATION_ESCAPE;
+    }
+}
